066290177b20d7a02b4fb2fd4252876672065b9e
hsiang hsu <skypassion5000@gmail.com>
2026-03-30 23:54:40 +0800

    調整活動頁 CTA 文案

--- app/src/app/product/[slug]/page.tsx
+++ app/src/app/product/[slug]/page.tsx
@@ -207,7 +207,7 @@ export default async function ProductPage({ params }: PageProps) {
 											d='M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75'
 										/>
 									</svg>
-									洽詢活動資訊
+									聯絡主辦單位
 								</a>
 								{product.catalogLink && (
 									<a
@@ -229,7 +229,7 @@ export default async function ProductPage({ params }: PageProps) {
 												d='M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3'
 											></path>
 										</svg>
-										下載活動附件
+										下載活動簡章
 									</a>
 								)}
 							</div>
